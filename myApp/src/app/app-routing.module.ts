79d2912bf1e4c0883d87eac2970d1ff3e4954512
ahmad-raza-vicenna <ahmad.raza@vicenna.com>
2022-01-18 13:05:11 +0500

    Routing

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './users/list/list.component';
+import { LoginComponent } from './users/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myApp/src/app/users/users.module.ts
+++ myApp/src/app/users/users.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ListComponent } from './list/list.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ListComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ListComponent
   ]
 })
 export class UsersModule { }
